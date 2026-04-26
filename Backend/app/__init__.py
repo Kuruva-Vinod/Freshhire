e8e9dca4f6b48feb635c277bc90047c49fc74273
Vinod <vinodkuruvavinod@gmail.com>
2026-04-26 08:29:46 +0530

    final fix

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,21 +1,20 @@
 from flask import Flask
 from .models import db
 from .routes import main
+import os
 
 def create_app():
     app = Flask(__name__)
 
-    import os
-
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'applications.db')    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'applications.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
     app.register_blueprint(main)
 
-    # 🔥 ADD THIS BLOCK HERE
+    # create DB tables
     with app.app_context():
         db.create_all()
 
