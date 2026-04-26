8d6564c3ffc757b43af6fe0f98558714b76fad26
Vinod <vinodkuruvavinod@gmail.com>
2026-04-26 08:07:45 +0530

    fix db for render

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask
-from flask_cors import CORS
 from .models import db
+from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///applications.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
-    from app.routes import main   # 👈 IMPORTANT change
     app.register_blueprint(main)
+
+    # 🔥 ADD THIS BLOCK HERE
+    with app.app_context():
+        db.create_all()
 
     return app
