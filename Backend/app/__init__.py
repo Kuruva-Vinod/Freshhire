9fb10e21d81003836c6a607a57fa3b3ca87e8f5f
Vinod <vinodkuruvavinod@gmail.com>
2026-04-26 08:17:25 +0530

    fixing error

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -5,8 +5,11 @@ from .routes import main
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///applications.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    import os
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'applications.db')    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
